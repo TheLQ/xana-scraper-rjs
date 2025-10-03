807ab0e4376ab050c321f40e7ee85ef2a3ee986c
Leon Blakey <code@leonblakey.com>
2025-10-03 01:20:55 -0700

    working scraper

--- scraper.gm.js
+++ scraper.gm.js
@@ -2,32 +2,92 @@
 // @name        test script
 // @namespace   Violentmonkey Scripts
 // @match       *://xana.sh/*
-// @grant GM_download
+// @grant       GM_xmlhttpRequest
 // @version     1.0
 // @author      -
 // @description 10/1/2025, 3:13:37 PM
 // @run-at document-end
 // ==/UserScript==
 
-console.log("hello")
-GM_download({
-    url: "https://",
-    name: "600369.torrent",
-    onload: (e) => {
-        console.log("hi onload");
-        console.log("r", e)
-    }
-})
+console.log("init scraper")
 
 // Create WebSocket connection.
-const socket = new WebSocket("ws://localhost:8080");
-
-// Connection opened
+const socket = new WebSocket("ws://desk.f.xana.sh:8080");
 socket.addEventListener("open", (event) => {
-    socket.send("Hello Server!");
+    socket.send(encodeOp(OP_INIT, document.location));
 });
-
-// Listen for messages
 socket.addEventListener("message", (event) => {
-    console.log("Message from server ", event.data);
+    // console.log("raw request: " + event.data);
+    let [op, data] = split_once(event.data, ":");
+    console.log(`received op ${op} data ${data}`);
+    switch (op) {
+        case OP_MESSAGE:
+            console.log("message", data);
+            break;
+        case OP_SCRAPE:
+            console.log("scrape", data);
+            scrape(data);
+            break;
+        default:
+            console.log("unknown op", op);
+    }
 });
+socket.addEventListener("close", (event) => {
+    console.log("Connection closed", event);
+});
+socket.addEventListener("error", (event) => {
+    console.log(`ws error`, event);
+})
+
+//
+
+const OP_INIT = "init";
+const OP_MESSAGE = "message";
+const OP_SCRAPE = "scrape";
+const OP_CONTENT = "content";
+
+function encodeOp(op, data) {
+    return op + ":" + data;
+}
+
+//
+
+function scrape(url) {
+    GM_xmlhttpRequest({
+        url,
+        responseType: "blob",
+        onload: (e) => {
+            socket.send(encodeOp(OP_CONTENT, e.status + ":" + e.responseHeaders));
+            socket.send(e.response);
+        },
+        onerror: (e) => {
+            console.log("scrape error", e);
+        },
+    });
+}
+
+//
+
+/** stupid javascript */
+function split_once(string, separator) {
+    let pos = string.indexOf(separator);
+    return [
+        string.substring(0, pos),
+        string.substring(pos + 1),
+    ]
+}
+
+// class EncodeOp {
+//     #op;
+//
+//     constructor(op) {
+//         this.op = op;
+//     }
+//
+//     encode(data) {
+//         return this.#op + ":" + data;
+//     }
+// }
+//
+// const INIT_OP = new EncodeOp("init");
+// const OP = new EncodeOp("start_page");
--- src/ws_server.rs
+++ src/ws_server.rs
@@ -1,8 +1,10 @@
+use bytes::BytesMut;
 use futures_util::{SinkExt, StreamExt};
+use std::fmt::{Display, Formatter};
 use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
 use tokio::net::{TcpListener, TcpStream};
-use tokio_websockets::{ServerBuilder, WebSocketStream};
+use tokio_websockets::{Config, Limits, Message, ServerBuilder, WebSocketStream};
 use xana_commons_rs::SimpleIoResult;
 use xana_commons_rs::tracing_re::info;
 
@@ -11,22 +13,97 @@ pub async fn start_web_socket_server(port: u16) -> SimpleIoResult<()> {
     let listener = TcpListener::bind(addr).await.expect("failed to bind");
     info!("listening on {addr}");
 
-    while let Ok((stream, _)) = listener.accept().await {}
+    while let Ok((stream, _addr)) = listener.accept().await {
+        tokio::spawn(async move { client_connection(stream).await });
+    }
 
     Ok(())
 }
 
 async fn client_connection(tcp_stream: TcpStream) {
+    info!("client connected from {}", tcp_stream.peer_addr().unwrap());
     let (_request, mut server) = ServerBuilder::new()
+        .config(Config::default().frame_size(usize::MAX))
+        .limits(Limits::unlimited())
         .accept(tcp_stream)
         .await
         .expect("failed to accept");
 
-    // Just an echo server, really
-    while let Some(msg_raw) = server.next().await {
-        let msg = msg_raw.expect("failed to unwrap msg");
-        if msg.is_text() || msg.is_binary() {
-            server.send(msg).await.expect("failed to send");
+    let mut expect_download_content = false;
+    while let Some(response_res) = server.next().await {
+        let message_raw = response_res.expect("failed to unwrap msg");
+
+        if expect_download_content {
+            assert!(message_raw.is_binary());
+            let content: BytesMut = message_raw.into_payload().into();
+            info!("received content of size {}", content.len());
+            expect_download_content = false;
+        } else {
+            let message = message_raw.as_text().expect("expected text message");
+            let op = match ClientOp::parse(message) {
+                ClientOp::Init { current_page } => {
+                    info!("client init at {}", current_page);
+
+                    Some(ServerOp::Scrape {
+                        url: "https://xana.sh/Aelitasupercomputer.jpg".into(),
+                    })
+                }
+                ClientOp::Content { headers_raw } => {
+                    info!("client headers: {headers_raw}");
+                    expect_download_content = true;
+                    None
+                }
+            };
+
+            if let Some(op) = op {
+                info!("requesting {}", op);
+                server
+                    .send(Message::text(op.encode()))
+                    .await
+                    .expect("failed to send");
+            } else {
+                info!("no follow up op");
+            }
+        }
+    }
+}
+
+enum ClientOp {
+    Init { current_page: String },
+    Content { headers_raw: String },
+}
+
+impl ClientOp {
+    fn parse(raw: &str) -> Self {
+        let (op, data_str) = raw.split_once(":").expect("bad op");
+        let data = data_str.to_string();
+
+        match op {
+            "init" => Self::Init { current_page: data },
+            "content" => Self::Content { headers_raw: data },
+            unknown => panic!("unknown op {}", unknown),
+        }
+    }
+}
+
+enum ServerOp {
+    Scrape { url: String },
+}
+
+impl ServerOp {
+    fn encode(&self) -> String {
+        match self {
+            Self::Scrape { url } => {
+                format!("scrape:{}", url)
+            }
+        }
+    }
+}
+
+impl Display for ServerOp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Scrape { url } => write!(f, "ScrapeOp for {url}"),
         }
     }
 }
